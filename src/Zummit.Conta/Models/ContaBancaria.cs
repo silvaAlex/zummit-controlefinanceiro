aaeea095dff2983d1289b1eb948a5db7a18df627
alex.external <silva_jralex@hotmail.com>
2023-12-17 22:35:38 -0300

    adicionando o Modelo

---
+++ src/Zummit.Conta/Models/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Zummit.ControleFinanceiro.API.Models.Banco
+{
+    public record ContaBancaria
+    {
+        private double saldo = 0.0;
+        private readonly DateTime dataAbertura;
+
+        public ContaBancaria()
+        {
+            saldo = 0.0;
+            dataAbertura = DateTime.Now;
+        }
+
+        public string RetornaSaldoFormatado()
+        {
+            return string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", saldo);
+        }
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public void AtualizarSaldo(double quantia)
+        {
+            saldo = quantia;
+        }
+
+        public string RetornaDataAberturaFormatada()
+        {
+            return dataAbertura.ToString("dd/mm/yyyy");
+        }
+    }
+}
